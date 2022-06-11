e39b0f2d87ddf9bbf0931d89bc1aa24ae48ec0bb
jonataslaet <jonataslaetprogramador@gmail.com>
2022-06-11 09:32:26 -0300

    data-structures
    Operations in the vector are now implemented
    All unit tests of Vector are passing

--- data-structures/java/src/main/com/github/jonataslaet/programmingchallenges/datastructures/Vector.java
+++ data-structures/java/src/main/com/github/jonataslaet/programmingchallenges/datastructures/Vector.java
@@ -1,35 +1,43 @@
 package main.com.github.jonataslaet.programmingchallenges.datastructures;
 
-public class Vector {
+public class Vector implements Operations {
 	
 	private Node[] nodes = new Node[100];
 	private int quantityOfNodes;
 
 	public void add(Node node) {
 		// Add a certain node at the end of the list
+		this.nodes[this.quantityOfNodes++] = node;
 	}
 
 	public void add(int position, Node node) {
 		// Add a certain node at the position of the list
+		this.nodes[position] = node;
 	}
 
 	public Node get(int position) {
 		// Get a certain node from a certain position of the list
-		return null;
+		return this.nodes[position];
 	}
 
 	public void remove(int position) {
 		// Remove a certain node from a certain position of the list
+		this.nodes[position] = null;
 	}
 
 	public boolean contains(Node node) {
 		// Verify if a certain node is in the list
+		for (int i = 0; i < nodes.length; i++) {
+			if (nodes[i] != null && node != null && node.equals(nodes[i])) {
+				return true;
+			}
+		}
 		return false;
 	}
 
 	public int size() {
 		// Get the quantity of nodes in the list
-		return 0;
+		return this.quantityOfNodes;
 	}
 
 	public String toString() {
