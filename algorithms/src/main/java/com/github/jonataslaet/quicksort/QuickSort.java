3c36af88b7422644909e30896f6e541e61e606f1
jonataslaet <jonataslaetprogramador@gmail.com>
2023-04-04 08:10:18 -0300

    algorithms: Quicksort

---
+++ algorithms/src/main/java/com/github/jonataslaet/quicksort/QuickSort.java
@@ -0,0 +1,37 @@
+package com.github.jonataslaet.quicksort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuickSort {
+    public static List<Integer> getOrderedNumbers(List<Integer> numbers) {
+        if (numbers.size() < 2) {
+            return numbers;
+        }
+        else if (numbers.size() == 2) {
+            if (numbers.get(0) > numbers.get(1)) {
+                int help = numbers.get(0);
+                numbers.set(0, numbers.get(1));
+                numbers.set(1, help);
+                return numbers;
+            }
+        } else {
+            int pivot = (numbers.size() / 2);
+            List<Integer> leftList = new ArrayList<>();
+            List<Integer> rightList = new ArrayList<>();
+            for (Integer number : numbers) {
+                if (number < numbers.get(pivot)) {
+                    leftList.add(number);
+                } else if (number > numbers.get(pivot)) {
+                    rightList.add(number);
+                }
+            }
+            List<Integer> newList = new ArrayList<>();
+            newList.addAll(getOrderedNumbers(leftList));
+            newList.add(numbers.get(pivot));
+            newList.addAll(getOrderedNumbers(rightList));
+            return newList;
+        }
+        return numbers;
+    }
+}
