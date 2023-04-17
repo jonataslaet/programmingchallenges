9f40427210f962ffd3cdb4bf1b201bad96945351
jonataslaet <jonataslaetprogramador@gmail.com>
2023-04-16 22:29:44 -0300

    algorithms: Longest Common Substring

---
+++ algorithms/src/main/java/com/github/jonataslaet/lcs/Main.java
@@ -0,0 +1,9 @@
+package com.github.jonataslaet.lcs;
+
+public class Main {
+    public static void main(String[] args) {
+        String word = "blue";
+        String word2 = "clues";
+        System.out.println("Longest Common Substring = " + LongestCommonSubstring.getLCS(word, word2));
+    }
+}
