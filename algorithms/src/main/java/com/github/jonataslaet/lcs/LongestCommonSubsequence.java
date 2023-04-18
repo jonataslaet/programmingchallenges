5ea8999dafa77e14b86533af9fcdda09510bf0cb
jonataslaet <jonataslaetprogramador@gmail.com>
2023-04-17 22:15:03 -0300

    algorithms: Longest Common Subsequence

---
+++ algorithms/src/main/java/com/github/jonataslaet/lcs/LongestCommonSubsequence.java
@@ -0,0 +1,21 @@
+package com.github.jonataslaet.lcs;
+
+public class LongestCommonSubsequence {
+    public static Integer getLCS(String firstWord, String secondWord) {
+        int firstWordLength = firstWord.length();
+        int secondWordLength = secondWord.length();
+        char[] w1 = firstWord.toCharArray();
+        char[] w2 = secondWord.toCharArray();
+        int [][] grid = new int[1+firstWordLength][1+secondWordLength];
+        for (int i = 1; i <= firstWordLength; i++) {
+            for(int j = 1; j <= secondWordLength; j++) {
+                if (w1[i-1] == w2[j-1]) {
+                    grid[i][j] = grid[i-1][j-1] + 1;
+                } else {
+                    grid[i][j] = Math.max(grid[i-1][j], grid[i][j-1]);
+                }
+            }
+        }
+        return grid[firstWordLength][secondWordLength];
+    }
+}
--- algorithms/src/main/java/com/github/jonataslaet/lcs/Main.java
+++ algorithms/src/main/java/com/github/jonataslaet/lcs/Main.java
@@ -5,5 +5,6 @@ public class Main {
         String word = "blue";
         String word2 = "clues";
         System.out.println("Longest Common Substring = " + LongestCommonSubstring.getLCS(word, word2));
+        System.out.println("Longest Common Subsequence = " + LongestCommonSubsequence.getLCS(word, word2));
     }
 }
