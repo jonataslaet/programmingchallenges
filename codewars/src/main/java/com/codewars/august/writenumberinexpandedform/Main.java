34a5208ec0969dc0768e4ac69fe6036d5a629dfa
jonataslaet <jonataslaetprogramador@gmail.com>
2023-08-22 21:00:48 -0300

    codewars: solutions made on august
    https://www.codewars.com/kata/5842df8ccbd22792a4000245/train/java

---
+++ codewars/src/main/java/com/codewars/august/writenumberinexpandedform/Main.java
@@ -0,0 +1,14 @@
+package com.codewars.august.writenumberinexpandedform;
+
+public class Main {
+    public static void main(String[] args){
+//        int num = 12;
+//        int num = 42;
+//        int num = 70304;
+//        int num = 10;
+//        int num = 1010;
+        int num = 101200;
+        String result = Kate.expandedForm(num);
+        System.out.println("Result = " + result);
+    }
+}
