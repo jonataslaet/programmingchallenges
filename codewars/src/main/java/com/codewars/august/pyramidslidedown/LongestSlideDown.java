49e0b44e54ad567f167d1207a5872a02038113b9
jonataslaet <jonataslaetprogramador@gmail.com>
2023-08-27 00:23:14 -0300

    codewars: fix solutions made on august
    https://www.codewars.com/kata/551f23362ff852e2ab000037/train/java

--- codewars/src/main/java/com/codewars/august/pyramidslidedown/LongestSlideDown.java
+++ codewars/src/main/java/com/codewars/august/pyramidslidedown/LongestSlideDown.java
@@ -3,14 +3,14 @@ package com.codewars.august.pyramidslidedown;
 //https://www.codewars.com/kata/551f23362ff852e2ab000037/train/java
 public class LongestSlideDown {
     public static int longestSlideDown(int[][] pyramid) {
-        int [][] pd = new int[999][999];
+        int [][] sum = new int[99][99];
         int size = pyramid.length;
         for (int i = size - 1; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
-                if (i == size - 1) pd[i][j] = pyramid[i][j];
-                else pd[i][j] = pd[i][j]  + Math.max(pd[i+1][j], pyramid[i+1][j+1]);
+                if (i == size - 1) sum[i][j] = pyramid[i][j];
+                else sum[i][j] = pyramid[i][j]  + Math.max(sum[i+1][j], sum[i+1][j+1]);
             }
         }
-        return pd[0][0];
+        return sum[0][0];
     }
 }
