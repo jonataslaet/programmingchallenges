1406b079330ac80fa20010240051158a331f605c
Jonatas Laet <jonataslaetprogramador@gmail.com>
2024-07-27 00:13:44 -0300

    Created selection sort algorithm

--- algorithms/src/main/java/com/github/jonataslaet/mycodeschool/Sorting.java
+++ algorithms/src/main/java/com/github/jonataslaet/mycodeschool/Sorting.java
@@ -5,6 +5,17 @@ import java.util.List;
 public class Sorting {
 
     public static void selectionSort(List<Integer> v) {
-        
+        int iMin = 0;
+        for (int i = 0; i < v.size() - 1; i++) {
+            for (int j = i; j < v.size(); j++) {
+                if (v.get(j) < v.get(iMin)) {
+                    iMin = j;
+                }
+            }
+            int temp = v.get(iMin);
+            v.set(iMin, v.get(i));
+            v.set(i, temp);
+            int a = 1;
+        }
     }
 }
