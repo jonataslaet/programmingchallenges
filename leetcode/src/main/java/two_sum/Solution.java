2e865df76094fc57f5efd5e232cde33382746680
Jonatas Laet <jonataslaetprogramador@gmail.com>
2025-02-01 11:56:12 -0300

    Refactored a solution

--- leetcode/src/main/java/two_sum/Solution.java
+++ leetcode/src/main/java/two_sum/Solution.java
@@ -2,9 +2,6 @@ package two_sum;
 
 /*
 Description: https://leetcode.com/problems/two-sum/
-Runtime: 5 ms, faster than 60.21% of Java online submissions for Two Sum.
-Memory Usage: 46.9 MB, less than 5.93% of Java online submissions for Two Sum.
-Time Submitted: 03/05/2022 00:31
 */
 
 import java.util.HashMap;
@@ -12,19 +9,17 @@ import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int missing = target - nums[0];
+        int missing;
 		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 		for (int i = 0; i < nums.length; i++) {
-			map.put(nums[i], i);
-		}
-		
-		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(nums[i])) {
+				return new int[] { map.get(nums[i]), i };
+			}
 			missing = target - nums[i];
-			if (map.containsKey(missing) && i != map.get(missing)) {
-				return new int[] { i, map.get(missing) };
+			if (!map.containsKey(missing)) {
+				map.put(missing, i);
 			}
 		}
-
 		return null;
     }
 }
