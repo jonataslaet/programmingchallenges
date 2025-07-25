3bf2fa440aa4c38b3551b6fc94fe21ab4b7f890f
Jonatas Laet <jonataslaetprogramador@gmail.com>
2025-07-24 22:41:33 -0300

    algorithms
    Refactored 1 algorithm: efficient prime.

--- algorithms/src/main/java/com/github/jonataslaet/beecrowd/Solution1221.java
+++ algorithms/src/main/java/com/github/jonataslaet/beecrowd/Solution1221.java
@@ -35,4 +35,23 @@ public class Solution1221 {
         }
         return true;
     }
+
+    // n is not prime if it can be written as the product of two natural numbers which are greater than one and obsviously less than n
+    // 1 < d < n
+    // (a * d) == n
+    // n = a * d; a pertence [2, n-1]
+    // d = n / a
+    // a <= d
+    // a <= (n / a)
+    // a² <= n
+    // a <= sqrt(n) E a >= 2
+    private static boolean isPrime2(int n) {
+        if (n < 2) return false;
+        double r = Math.floor(Math.sqrt(n));
+        int sqrtN = (int) Math.sqrt(n);
+        for (int i = 2; i <= sqrtN; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
 }
